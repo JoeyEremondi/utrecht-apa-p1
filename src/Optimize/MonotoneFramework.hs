8d00b1158106240be48be4adcddc2d38a864f56b
jmite <jmitdase@gmail.com>
2015-02-11 23:46:37 +0100

    Initial draft of worklist algo

--- src/Optimize/MonotoneFramework.hs
+++ src/Optimize/MonotoneFramework.hs
@@ -5,29 +5,73 @@
 module Optimize.MonotoneFramework where
 
 import qualified Data.Set as Set
+import Data.Ix
+import Data.Array.ST
+import Control.Monad
+import Control.Monad.ST
+import qualified Data.Map as Map
+import qualified Data.Array as Array
 
 --Wrapper for a control flow graph
 newtype CFG block = CFG (Set.Set (block, block))
 
+data ProgramInfo label = ProgramInfo {
+  edgeMap :: label -> [label],
+  labelRange :: (label, label),
+  allLabels :: [label],
+  labelPairs :: [(label, label)],
+  isExtremal :: label -> Bool
+  
+  }
+
 incoming :: (Ord block) => CFG block -> block -> Set.Set block
 incoming (CFG controlFlow) l = Set.map snd $ Set.filter ((== l) . fst) controlFlow
 
-class (Ord a) => CompleteLattice a where
+class (Ord a, Eq a) => CompleteLattice a where
   latticeTop :: a
   latticeBottom :: a
   latticeJoin :: a -> a -> a
-  (|^|) :: a -> a -> a --infix notation for join
   iota :: a --Extremal value for our analysis
+  lleq :: a -> a -> bool
+
+
 
 joinAll :: (CompleteLattice a) => Set.Set a -> a
 joinAll = Set.foldr latticeJoin latticeBottom
 
-class (Ord block) => MonotoneFramework block where
-    --We either reverse all labels, or don't reverse them
-    makePairs :: CFG block -> CFG block
-    extremalBlocks :: Set.Set block
-
-
+--worklist algo for least fixed point
+minFP :: (CompleteLattice payload, Ord label) =>
+         (payload -> payload)
+         -> ProgramInfo label
+         -> (Map.Map label payload, Map.Map label payload) 
+minFP f info = (mfpOpen, mfpClosed)
+  where
+    mfpClosed = Map.map f mfpOpen
+    --stResult :: ST s [(label, payload)]
+    initialSolns = foldr (\l solnsSoFar ->
+                             if isExtremal info l
+                             then Map.insert l latticeBottom solnsSoFar
+                             else Map.insert l iota solnsSoFar
+                           ) Map.empty (allLabels info)
+    mfpOpen = iterateSolns initialSolns (labelPairs info)
+    iterateSolns currentSolns [] = currentSolns
+    iterateSolns currentSolns ((l,l'):rest) = let
+      al = currentSolns Map.! l
+      al' = currentSolns Map.! l'
+      fal = f al
+      (newPairs, newSolns) =
+        if (fal `lleq` al') 
+        then let
+            theMap = Map.empty --Map.insert l' (join al' fal) currentSolns
+            thePairs = map (\lNeighbour -> (l', lNeighbour) ) $ edgeMap info l'
+          in (thePairs, theMap)
+        else ([], currentSolns)
+      in iterateSolns newSolns (newPairs ++ rest)
+        
+      
+      
+    
+          
 
 
 
