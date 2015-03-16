4d0ea07500f6c227ab534bd80f2352436b953717
jmite <jmitdase@gmail.com>
2015-03-16 20:19:02 +0100

    Added imperative test

---
+++ apaTests/ImperativeTest.elm
@@ -0,0 +1,37 @@
+module ImperativeTest where
+
+import State.Escapable (..)
+import Text (..)
+import Graphics.Element (..)
+
+basicStateTest : Int -> Maybe a -> EState ph Int
+basicStateTest x y =
+  (if (x <= 0)
+    then newRef 1 
+    else newRef x) `andThen` \ref ->
+  deRef ref `andThen` \newX ->
+  writeRef ref (2*newX) `andThen` \_ ->
+  (case y of
+    Just _ -> writeRef ref (3*newX)
+    Nothing -> writeRef ref (4*newX))
+      `andThen` \_ ->
+  deRef ref
+
+stateFn : Int -> StateRef ph Int -> EState ph {}
+stateFn x ref =
+  deRef ref `andThen` \oldVal -> 
+  basicStateTest x (Just 20) `andThen` \fnRet ->
+  writeRef ref (fnRet + oldVal) `andThen` \_ ->
+  liftToState {}
+
+stateFnCaller : Int -> EState ph Int
+stateFnCaller x = 
+  newRef 100 `andThen` \ref ->
+  stateFn x ref `andThen` \_ ->
+  deRef ref 
+
+main = 
+  flow down
+    [ asText <| runState <| basicStateTest 10 (Just 20),
+      asText <| runState <| stateFnCaller 20
+    ]
